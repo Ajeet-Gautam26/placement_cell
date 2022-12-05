b60f0a3b736b6702289243d99cd55ac01bf7eda4
Ajeet Gautam <Ajeetgautam26@gmail.com>
2022-12-05 18:50:46 +0530

    first coment placement cell

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'config/.env' });
+//const MONGODB_URI = 'mongodb+srv://${username}:${password}@placementcluster.4kl7gjo.mongodb.net/placementDB?retryWrites=true&w=majority';
+//connecting mongoose with database
+//I stored MONGODB_URI in my system veriable for security reason. veriable name MONGODB_URI followed by your mongo atlas link
+//for local use you can write this code
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/csvUploader');
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = db;
